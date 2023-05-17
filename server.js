f4dfd6b19ed4c9acaf195156d889992e273efd87
Abdessittir <harkati.web.dev@gmail.com>
2023-05-17 17:47:00 +0100

    Finish project setup using docker-compose

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { PrismaClient } from '@prisma/client'
-
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
@@ -11,10 +10,10 @@ const prisma = new PrismaClient();
 const users = await prisma.user.findMany();
 
 app.get('/', (req, res) => {
-    res.status(200).send({message: 'Hello world'});
+    res.status(200).send({ message: 'Hellow world' });
 });
 
 const port = process.env.PORT ?? 8080;
 app.listen(port, () => {
-    console.log('server runing on 8080');
+    console.log('Server lisning on port', port);
 });
