334594bd57ab9fb523ae92b5275eaa6dc71dab5e
yonas44 <tesfu.yonas1350@gmail.com>
2023-05-22 16:22:42 +0300

    Create React project with vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const fetchData = async() => {
+    const req = await axios.get('http://localhost:8080')
+  }
+
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className="text-red-500 text-6xl">Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
